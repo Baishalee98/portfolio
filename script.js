0e56fb4625a19f280466850f7f2ddc5e27331f3b
Baishalee Paul <baishalee98@gmail.com>
2020-07-25 21:14:38 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+/*
+const menuBar = document.querySelector('.nav_btn');
+
+showMenu = false;
+
+$(".nav_btn").click(function() {
+    
+    if(showMenu) {
+        menuBar.classList.add('close');
+    }
+    
+});
+
+
+$(".click_action").click(function() {
+    var flag = $("#navbar_content").is(":visible");
+    if(flag == true){
+        $("#myNavbar").css("background","transparent");
+    }else{
+        $("#myNavbar").css("background-color","black");
+    }
+});
+*/
